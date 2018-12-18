a557908a7f9fe1ef95bcd947533c3bcd790477d5
user <user@192.168.1.5>
2018-12-18 17:59:55 +0530

    first commit

---
+++ WishesApp/src/main/java/com/app/WishesApp/WishesAppApplication.java
@@ -0,0 +1,14 @@
+package com.app.WishesApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishesAppApplication.class, args);
+	}
+
+}
+
